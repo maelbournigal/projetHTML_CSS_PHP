6dd6c79bcf751bf95edc942f7eb6fdd0f9248844
Samuel Poultier <samuel.poultier@ynov.com>
2017-04-13 23:59:49 +0200

    ajout commentaire

--- php/accueil.php
+++ php/accueil.php
@@ -1,26 +1,22 @@
-<!-- Page réalisé par Samuel Poultier -->
-<html>
+<!--SAMUEL POULTIER | RESOLUTION :1920*1080 -->
+ <html>
   <head>
-    <meta charset="utf-8">
-    <title>Ma²Saje agence web</title>
-    <link href="../css/cssprojet.css" rel="stylesheet"/>
-    <link href="https://fonts.googleapis.com/css?family=Concert+One" rel="stylesheet">
+    <meta charset="utf-8"> <!--encodage permettant de bien lire les accents et caractères spéciaux-->
+    <title>Ma²Saje agence web</title>  <!--Titre de l'onglet sur le navigateur-->
+    <link href="../css/cssprojet.css" rel="stylesheet"/> <!-- on lie le css à la page-->
+    <link href="https://fonts.googleapis.com/css?family=Concert+One" rel="stylesheet"> <!--Import de la police Concert One 
+    source: google font-->
     </head>
     <body>
         <?php include('../headerprojet.html'); 
         include('../menu.html') ?>
         <img src="../img/startup-palace.jpg" alt="bureau de notre startup" title="nosbureaux" />
+        <!--On utilise des div et id pour bien positionner les élements-->
         <div id="legende1"> 
             <h1 id="titrelegende">Ma²saje</h1>
             <h2 id="titrelegende2">Agence de communication digitale.</h2> <br>
             <p id="paragraphelegende">Nous réalisons les site web de TPE et PME désirant un concept innovant et personalisé.<br>Grâce à la méthode
             Agile nous échangons régulièrement avec le client. <br>Basés à Nantes nous travaillons sur la Loire-Atlantique.</p>
-<!--
-    <footer>
-     <p> Ma<sup>2</sup>Sage- 3 Rue des Logis - 44430 La Remaudière, France </p>
-     <p>Mail: contact@ma2sage.com Téléphone : 02 47 12 63 19</p> 
-    </footer>
--->
         </div>
     </body>
 </html>
